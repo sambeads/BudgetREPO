07b9b8a1b60e0c75349fcbf2d911818fa29638c3
Sam Beadles <sam.beadles@oliverwyman.com>
2019-01-08 08:28:43 -0500

    Eliminated some comments, random pytest testing things

--- skeleton.py
+++ skeleton.py
@@ -18,16 +18,6 @@ Things I can do:
     () Use SSH keys
 '''
 
-
-def mainer():
-    pass
-
-# function that takes the excel file and parses to find the right bits:
-# date, amount, description.
-# Also inputs 'Chase' as type. Has columns that are empty for type and subtype.
-# Leaves some space at the top.
-
-
 #@pytest.fixture()
 def read_from_excel():
     filepath_of_original_doc = output_file_path(original=True)
@@ -41,19 +31,6 @@ def read_from_excel():
 
 
 def add_spaces_to_dataframe(df):
-    # single_day_for_comparison = datetime.timedelta(days=1)
-
-    # list_of_separating_dates = []
-    # import ipdb; ipdb.set_trace()
-    # for i in range((len(df)-1)):
-    #     # import ipdb; ipdb.set_trace()
-    #     datesMorethanaDayApart = df.loc[i,'Date'] - df.loc[i+1,'Date'] < -single_day_for_comparison
-    #     if datesMorethanaDayApart:
-    #         list_of_separating_dates = list_of_separating_dates.append(df.loc[i,'Date'])
-
-    # Set the index of the original df and the missing dates df to 'Date'
-
-    # new idea:
     start_day = df.iloc[1]['Date']
     end_day = df.iloc[-1]['Date']
 
@@ -75,6 +52,7 @@ def output_file_path(original=True):
     account = 'Chase9075'
     log = 'RecentActivity'
     time = datetime.datetime.now().strftime("%Y%m%d")
+   
     # the string is Chase9075_RecentActivity_[datetime]
     fstringer = f"{path}{account}_{log}_{time}.csv"
     fstringer_cleaned = f"{path}cleaned_{account}_{log}_{time}.csv"
@@ -114,31 +92,6 @@ def write_to_csv(df):
     filepath_of_original_doc = output_file_path(original=True)
     filepath_of_cleaned_doc = output_file_path(original=False)
     df.to_csv(filepath_of_cleaned_doc, index=False)
-
-
-def parsing_excel():
-    pass
-
-
-def read_from_pdf():
-    # function that reads from PDF
-    pass
-
-
-def increment(num):
-    return num + 1
-
-
-def broken_increment(num):
-    if num == 0:
-        return 'zero'
-    elif num < 0:
-        return 'negative'
-    elif num > 1:
-        return 'Positive, greater than zero (wrong)'
-    else:
-        return 'agh!'
-
 
 if __name__ == '__main__':
     mainer()
